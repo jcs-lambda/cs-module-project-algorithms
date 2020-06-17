4457daf6edd1e5d3c20538e75ee990dd08580e0f
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-06-17 13:27:26 -0500

    implements moving_zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,10 +3,11 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
-
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr.insert(0, arr.pop(i))
+    
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
