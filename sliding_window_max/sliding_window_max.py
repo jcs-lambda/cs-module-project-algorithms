95dcf2d62f3cd9fcb59506e2f489801b00f92270
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-06-17 17:20:51 -0500

    refactor sliding_window_max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,14 +2,39 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+
 def sliding_window_max(nums, k):
-    maxes = [
-        max(nums[i:i+k])
-        for i in range(len(nums) - k + 1)
-    ]
+    # initialize first window
+    window = nums[:k]
+    # get first max value
+    current_max = max(window)
+    # store first max value
+    maxes = [current_max]
+
+    # iterate over the rest of the values
+    for i, x in enumerate(nums[k:]):
+        # add newest value to window
+        window.append(x)
+        # remove oldest value from window, and, 
+        # if it equals the current max,
+        # then recalculate the current max
+        if window.pop(0) == current_max:
+            current_max = max(window)
+        # oldest value wasn't max, check if newest value is max
+        elif x > current_max:
+            current_max = x
+        # store maximum for this window
+        maxes.append(current_max)
 
     return maxes
 
+
+    # maxes = [0] * (len(nums) - k + 1)
+    # for i in range(len(maxes)):
+    #     maxes[i] = max(nums[i:i+k])
+
+    # return maxes
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
